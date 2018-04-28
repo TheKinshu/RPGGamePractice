26129a2e4c23f358106f98b3cc9441a23a190da7
Kinshu <kc007919@gmail.com>
2018-04-28 02:49:34 -0400

    Combat System

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -1,55 +1,68 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour {
 
-    #region Singleton
-    public static Inventory instance;
+	#region Singleton
 
-    void Awake()
-    {
-        if (instance != null)
-        {
-            Debug.LogWarning("More than one instance of Inventory found!");
-            return;
-        }
+	public static Inventory instance;
 
-        instance = this;
-    }
-    #endregion
+	void Awake ()
+	{
+		if (instance != null)
+		{
+			Debug.LogWarning("More than one instance of Inventory found!");
+			return;
+		}
 
-    public delegate void OnItemChanged();
-    public OnItemChanged OnItemChangedCallback;
+		instance = this;
+	}
 
-    public int space = 20;
+	#endregion
 
-    public List<Item> items = new List<Item>();
+	// Callback which is triggered when
+	// an item gets added/removed.
+	public delegate void OnItemChanged();
+	public OnItemChanged onItemChangedCallback;
 
+	public int space = 20;	// Amount of slots in inventory
 
-    public bool Add(Item item)
-    {
-        if (!item.isDefaultItem)
-        {
-            if (items.Count >= space)
-            {
-                Debug.Log("Not enough room");
-                return false;
-            }
-            items.Add(item);
-            if (OnItemChangedCallback != null)
-            {
-                OnItemChangedCallback.Invoke();
-            }
-        }
-        return true;
-    }
-    public void Remove(Item item)
-    {
-        items.Remove(item);
-        if (OnItemChangedCallback != null)
-        {
-            OnItemChangedCallback.Invoke();
-        }
-    }
+	// Current list of items in inventory
+	public List<Item> items = new List<Item>();
+
+	// Add a new item. If there is enough room we
+	// return true. Else we return false.
+	public bool Add (Item item)
+	{
+		// Don't do anything if it's a default item
+		if (!item.isDefaultItem)
+		{
+			// Check if out of space
+			if (items.Count >= space)
+			{
+				Debug.Log("Not enough room.");
+				return false;
+			}
+
+			items.Add(item);	// Add item to list
+
+			// Trigger callback
+			if (onItemChangedCallback != null)
+				onItemChangedCallback.Invoke();
+		}
+
+		return true;
+	}
+
+	// Remove an item
+	public void Remove (Item item)
+	{
+		items.Remove(item);		// Remove item from list
+
+		// Trigger callback
+		if (onItemChangedCallback != null)
+			onItemChangedCallback.Invoke();
+	}
+
 }
--- Assets/Scripts/Inventory/InventoryUI.cs
+++ Assets/Scripts/Inventory/InventoryUI.cs
@@ -1,40 +1,49 @@
-﻿using UnityEngine;
+using UnityEngine;
+
+/* This object updates the inventory UI. */
 
 public class InventoryUI : MonoBehaviour {
 
-    public Transform itemsParent;
-    public GameObject inventoryUI;
-    Inventory inventory;
+	public Transform itemsParent;	// The parent object of all the items
+	public GameObject inventoryUI;	// The entire UI
 
-    InventorySlot[] slots;
+	Inventory inventory;	// Our current inventory
 
-    void Start () {
-        inventory = Inventory.instance;
-        inventory.OnItemChangedCallback += UpdateUI;
+	InventorySlot[] slots;	// List of all the slots
 
-        slots = itemsParent.GetComponentsInChildren<InventorySlot>();
+	void Start () {
+		inventory = Inventory.instance;
+		inventory.onItemChangedCallback += UpdateUI;	// Subscribe to the onItemChanged callback
+
+		// Populate our slots array
+		slots = itemsParent.GetComponentsInChildren<InventorySlot>();
 	}
 	
-	// Update is called once per frame
 	void Update () {
-        if (Input.GetButtonDown("Inventory"))
-        {
-            inventoryUI.SetActive(!inventoryUI.activeSelf);
-        }
+		// Check to see if we should open/close the inventory
+		if (Input.GetButtonDown("Inventory"))
+		{
+			inventoryUI.SetActive(!inventoryUI.activeSelf);
+		}
 	}
 
-    void UpdateUI()
-    {
-        for (int i = 0; i < slots.Length; i++)
-        {
-            if (i < inventory.items.Count)
-            {
-                slots[i].AddItem(inventory.items[i]);
-            }
-            else
-            {
-                slots[i].ClearSlot();
-            }
-        }
-    }
+	// Update the inventory UI by:
+	//		- Adding items
+	//		- Clearing empty slots
+	// This is called using a delegate on the Inventory.
+	void UpdateUI ()
+	{
+		// Loop through all the slots
+		for (int i = 0; i < slots.Length; i++)
+		{
+			if (i < inventory.items.Count)	// If there is an item to add
+			{
+				slots[i].AddItem(inventory.items[i]);	// Add it
+			} else
+			{
+				// Otherwise clear the slot
+				slots[i].ClearSlot();
+			}
+		}
+	}
 }
